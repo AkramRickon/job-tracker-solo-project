3707f160552df9875cd8bf8582ddfcdb24e934bb
akramRickon <akramhsin29@gmail.com>
2023-01-12 21:40:05 +0600

    baclend authentication complete

---
+++ client/src/app/services/auth.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { User } from '../interfaces/User';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  authUrl = "http://localhost:3000/auth"
+  constructor(private httpClient: HttpClient) { }
+
+
+  proceedRegister(user: any) {
+    return this.httpClient.post<User>(`${this.authUrl}/signup`, user);
+  }
+
+  proceedLogin (loginData:any) {
+    return this.httpClient.post(`${this.authUrl}/signin`,loginData)
+  }
+
+}
